adbaced90e390f66a102b592493b6e206fc6d8d3
Andrey Karamnoff <karamnoff@gmail.com>
2019-02-02 12:01:39 +0300

    add exercise-unit for forms

---
+++ __tests__/test.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import url from 'url';
+import axios from 'axios';
+import { parseRequest } from 'http-string-parser';
+
+describe('request', () => {
+  const data = fs.readFileSync('solution', 'utf-8');
+  const requestObj = parseRequest(data);
+
+  const parts = {
+    port: 8080,
+    protocol: 'http',
+    hostname: 'localhost',
+    pathname: requestObj.uri,
+  };
+  const requestUrl = url.format(parts);
+
+  const options = {
+    method: requestObj.method,
+    headers: requestObj.headers,
+    data: requestObj.body,
+    url: requestUrl,
+  };
+
+  it('should work', async () => {
+    const { status, statusText } = await axios(options);
+    const result = { status, statusText };
+    expect(result).toMatchObject({ status: 200 });
+  });
+});
